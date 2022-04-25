43216c5a21fb1ef180e3d4dd2c8287e43c1e1acf
k-nikola <nikola.kurbalija@hotmail.com>
2022-04-25 16:48:03 +0200

    Restructured configuration, and references.

--- index.ts
+++ index.ts
@@ -8,10 +8,10 @@ let projectName = pulumi.getProject()
 let stackName = pulumi.getStack()
 
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup(`${projectName}${stackName}AppRG`);
+const resourceGroup = new resources.ResourceGroup(`${projectName}-${stackName}RG`);
 
 //Create storage account
-const storageAccount = new storage.StorageAccount(`${projectName}${stackName}AciSa`, {
+const storageAccount = new storage.StorageAccount(`${projectName}-${stackName}AciSa`, {
     resourceGroupName: resourceGroup.name,
     location: resourceGroup.location,
     accountName: `${projectName}${stackName}storageacc`,
@@ -29,32 +29,32 @@ const storageAccountKeys = storage.listStorageAccountKeysOutput({
 })
 const storageAccountKey = storageAccountKeys.keys[0].value
 
-const fileShare = new storage.FileShare(`${projectName}${stackName}share`, {
+const fileShare = new storage.FileShare(`${projectName}-${stackName}share`, {
     accountName: storageAccount.name,
     resourceGroupName: resourceGroup.name,
     shareQuota: 50
 })
 
-// Create a new container group with 1 container in it, expose port 5000
+// Create a new container group with 1 container in it, expose port 80
 const containerGroup = new containerinstance.ContainerGroup(`${projectName}${stackName}CG`, {
     resourceGroupName: resourceGroup.name,
     osType: config.require("os"),
     containers: [{
         name: "db",
-        image: config.require("dbImage"),
+        image: config.require("services.db.image"),
         resources: {
             requests: {
-                cpu: 0.5,
-                memoryInGB: 0.5,
+                cpu: config.requireNumber("services.db.specs.cpu"),
+                memoryInGB: config.requireNumber("services.db.specs.mem"),
             }
         },
         environmentVariables: [{
             name: "MYSQL_ROOT_PASSWORD",
-            secureValue: config.require("MYSQL_ROOT_PASSWORD")
+            secureValue: config.require("services.db.vars.MYSQL_ROOT_PASSWORD")
         },
         {
             name: "MYSQL_DATABASE",
-            secureValue: config.require("MYSQL_DATABASE")
+            secureValue: config.require("services.db.vars.MYSQL_DATABASE")
         },
         ],
         volumeMounts: [
@@ -66,31 +66,30 @@ const containerGroup = new containerinstance.ContainerGroup(`${projectName}${sta
         ],
     }, {
         name: "nginx-rproxy",
-        image: config.require("nginxImage"),
+        image: config.require("services.nginx.image"),
         ports: [{ port: 80, protocol: "Tcp" }],
         resources: {
             requests: {
-                cpu: 0.5,
-                memoryInGB: 0.5,
+                cpu: config.requireNumber("services.nginx.specs.cpu"),
+                memoryInGB: config.requireNumber("services.nginx.specs.mem"),
             }
         },
     }, {
-        name: "flask-app",
-        image: config.require("appImage"),
-        ports: [{ port: 5000, protocol: "Tcp" }],
+        name: "flaskapp",
+        image: config.require("service.flaskapp.image"),
         resources: {
             requests: {
-                cpu: 0.5,
-                memoryInGB: 0.5,
+                cpu: config.requireNumber("service.flaskapp.specs.cpu"),
+                memoryInGB: config.requireNumber("service.flaskapp.specs.ram"),
             }
         },
         environmentVariables: [{
             name: "db_uri",
-            secureValue: config.require("DB_URI")
+            secureValue: config.require("service.flaskapp.vars.DB_URI")
         },
         {
             name: "secret_key",
-            secureValue: config.require("SECRET_KEY")
+            secureValue: config.require("service.flaskapp.vars.SECRET_KEY")
         }
         ],
     }
