71e12ae4c281b97605d64d2c9227486a8a2c8248
k-nikola <nikola.kurbalija@hotmail.com>
2022-05-12 08:38:19 +0200

    Updated workflow to deploy dev stack, and preview prod and stage.

--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ export let projectName = pulumi.getProject()
 export let stackName = pulumi.getStack()
 
 // Create global resources - Resource Group.
-const globalResources = new appGlobalResources(projectName, stackName)
-const stackResourceGroup = globalResources.stackResourceGroup
+const stackGlobalResources = new appGlobalResources(projectName, stackName)
+const stackResourceGroup = stackGlobalResources.stackResourceGroup
 
 // Create storage for the app group.
 const stackStorage = new appStorage(projectName, stackName, stackResourceGroup)
