439991c08eba6542d22b66d7b8736aaa7f04bb2a
k-nikola <nikola.kurbalija@hotmail.com>
2022-05-13 10:05:37 +0200

    Small restructure.

--- global/config-loader.ts
+++ global/config-loader.ts
@@ -2,27 +2,29 @@ import * as pulumi from "@pulumi/pulumi";
 
 // Service configuration structure
 export interface serviceConfig {
-    [serviceName: string]: {
-        image: string;
-        specs: {
-            cpu: number;
-            mem: number;
-        };
-        vars: {
-            [varName: string]: string
-        };
-        opts?: any;
+    image: string;
+    specs: {
+        cpu: number;
+        mem: number;
     };
+    vars: {
+        [varName: string]: string
+    };
+    opts?: any;
+};
+
+export interface stackServicesConfig {
+    [serviceName: string]: serviceConfig
 }
 
 // Object of configLoader class can be used to load the configuration of the stack, and to load hierarchical service configuration.
 export class configLoader {
     public readonly config: pulumi.Config
-    public readonly stackServices: serviceConfig
+    public readonly stackServices: stackServicesConfig
 
     constructor() {
         this.config = new pulumi.Config()
-        this.stackServices = this.config.requireObject<serviceConfig>("services")
+        this.stackServices = this.config.requireObject<stackServicesConfig>("stackServices")
     }
 }
 
